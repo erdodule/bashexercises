ac132c389479c4c7e9fee1176e64cf97adaad17c
erdodule <erdo_dule@hotmail.com>
2021-09-24 11:15:51 +0200

    exercise3

---
+++ exercise3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Write a script, using $RANDOM, to write the following output both to a file and to a
+#variable. The following numbers are only to show the format; your script should
+#produce different numbers:
+#1988.2365
+#13798.14178
+#10081.134
+#3816.15098
+
+
+printf "%f\n" "$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM" > FILENAME
+erdo=$(<FILENAME)
+printf "$erdo\n"
+
+
+
+
